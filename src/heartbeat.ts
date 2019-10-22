4a1d9e7db256dcb5fee427748ceee29317980561
Tom Lee <thomasslee97@gmail.com>
2019-10-22 22:39:24 +0100

    feat: Heartbeat system info

--- src/heartbeat.ts
+++ src/heartbeat.ts
@@ -1,8 +1,29 @@
-import { HeartbeatCommand } from './heartbeat-commands/heartbeat-commands'
+import { HeartbeatCommand, HeartbeatCommandType } from './heartbeat-commands/heartbeat-commands'
 
 export interface Heartbeat {
-	time: Date
+	time: Date,
+	data?: HeartbeatDataAny[]
 }
+
+interface HeartbeatDataBase {
+	command: HeartbeatCommandType,
+	data: any
+}
+
+export interface SystemInfoData {
+	cpu_usage_percent: number,
+	ram_available: number,
+	ram_used: number,
+	disk_capacity: number,
+	disk_usage: number
+}
+
+export interface HeartbeatDataSystemInfo extends HeartbeatDataBase {
+	command: HeartbeatCommandType.SendSystemInfo,
+	data: SystemInfoData
+}
+
+export type HeartbeatDataAny = HeartbeatDataSystemInfo
 
 export interface HeartbeatResponse {
 	commands?: HeartbeatCommand[]
