cb22e54d895f31acc0b3bbcb68b32a4da0a1adb2
Tom Lee <thomasslee97@gmail.com>
2019-10-22 21:49:19 +0100

    feat: Send system info

--- src/__tests__/anser-worker.spec.ts
+++ src/__tests__/anser-worker.spec.ts
@@ -1,8 +1,74 @@
+import { HeartbeatCommandType, HeartbeatResponse, SystemInfoData } from 'anser-types'
 import { AnserWorker } from '../anser-worker'
+
+const MOCK_SYSTEM_INFO: SystemInfoData = {
+	cpu_usage_percent: 50,
+	disk_capacity: 1000,
+	disk_usage: 900,
+	ram_available: 2000,
+	ram_used: 1500
+}
+
+function mockGetSystemInfo (): Promise<SystemInfoData> {
+	return Promise.resolve(MOCK_SYSTEM_INFO)
+}
 
 describe('anser-worker', () => {
 	it ('Starts', () => {
 		const worker = new AnserWorker('dev-worker', '127.0.0.1:5001')
 		worker.Start()
+	})
+
+	it('Resolves false with no commands', async () => {
+		const worker = new AnserWorker('dev-worker', '127.0.0.1:5001');
+		(worker as any).getSystemInfo = mockGetSystemInfo
+		const commands: HeartbeatResponse = {
+			commands: []
+		}
+		const result = await (worker as any).processHeartbeatResponse(commands)
+		expect((worker as any)._nextHeartbeat.data)
+		.toEqual([])
+		expect(result).toBe(false)
+	})
+
+	it('Sends SystemInfo', async () => {
+		const worker = new AnserWorker('dev-worker', '127.0.0.1:5001');
+		(worker as any).getSystemInfo = mockGetSystemInfo
+		const commands: HeartbeatResponse = {
+			commands: [{
+				type: HeartbeatCommandType.SendSystemInfo
+			}]
+		}
+		let result = await (worker as any).processHeartbeatResponse(commands)
+		expect((worker as any)._nextHeartbeat.data)
+		.toEqual([
+			{
+				command: HeartbeatCommandType.SendSystemInfo,
+				data: {
+					cpu_usage_percent: 50,
+					disk_capacity: 1000,
+					disk_usage: 900,
+					ram_available: 2000,
+					ram_used: 1500
+				}
+			}
+		])
+		expect(result).toBe(true);
+		(worker as any)._nextHeartbeat = { time: new Date() }
+		result = await (worker as any).processHeartbeatResponse(commands)
+		expect((worker as any)._nextHeartbeat.data)
+		.toEqual([
+			{
+				command: HeartbeatCommandType.SendSystemInfo,
+				data: {
+					cpu_usage_percent: 50,
+					disk_capacity: 1000,
+					disk_usage: 900,
+					ram_available: 2000,
+					ram_used: 1500
+				}
+			}
+		])
+		expect(result).toBe(true)
 	})
 })
--- src/anser-worker.ts
+++ src/anser-worker.ts
@@ -1,4 +1,11 @@
+import { throwStatement } from '@babel/types'
+import { Heartbeat, HeartbeatCommandType, HeartbeatDataSystemInfo, HeartbeatResponse, SystemInfoData } from 'anser-types'
+import { sign } from 'crypto'
 import { get } from 'request-promise'
+import { fsSize, Systeminformation } from 'systeminformation'
+import { currentLoad } from 'systeminformation'
+import { mem } from 'systeminformation'
+import { diskLayout } from 'systeminformation'
 import { logger } from './logger/logger'
 
 const KEEPALIVE_INTERVAL = 1000
@@ -9,13 +16,19 @@ const KEEPALIVE_INTERVAL = 1000
  * @property controller: Hostname of this workers' controller.
  */
 export class AnserWorker {
+	private _protocol: string = 'https'
 	private _running: boolean = false
 	private _connected: boolean = false
+	private _nextHeartbeat: Heartbeat = { time: new Date(), data: [] }
 	constructor (
 		public id: string,
-		public controller: string
+		public controller: string,
+		public debug?: boolean
 	) {
-
+		/* istanbul ignore next */
+		if (this.debug) {
+			this._protocol = 'http'
+		}
 	}
 
 	/**
@@ -28,20 +41,65 @@ export class AnserWorker {
 
 	/* istanbul ignore next */
 	private keepAlive (): void { // TODO: Send heartbeat
-		get(`http://${this.controller}`).then(() => {
+		get(`${this._protocol}://${this.controller}/heartbeat/${this.id}`).then((data: HeartbeatResponse) => {
 			if (!this._connected) {
 				logger.info(`Connected to controller`)
 			}
 			this._connected = true
-		}).catch(() => {
+			this.processHeartbeatResponse(data)
+		}).catch((err) => {
 			if (this._connected) {
-				logger.info(`Disconnected from controller!`)
+				logger.info(`Disconnected from controller! ${err}`)
 			}
 			this._connected = false
 		})
 
 		if (this._running) {
 			setTimeout(() => { this.keepAlive() }, KEEPALIVE_INTERVAL)
+		}
+	}
+
+	private async processHeartbeatResponse (resp: HeartbeatResponse): Promise<boolean> {
+		if (resp.commands && resp.commands.length) {
+			const actions = resp.commands.map(async (command) => {
+				switch(command.type) {
+					case HeartbeatCommandType.SendSystemInfo:
+						const data: HeartbeatDataSystemInfo = {
+							command: HeartbeatCommandType.SendSystemInfo,
+							data: await this.getSystemInfo()
+						}
+						if (this._nextHeartbeat.data && this._nextHeartbeat.data.length >= 0) {
+							this._nextHeartbeat.data.push(data)
+						} else {
+							this._nextHeartbeat.data = [data]
+						}
+						break
+				}
+			})
+			await Promise.all(actions)
+			return Promise.resolve(true)
+		} else {
+			return Promise.resolve(false)
+		}
+	}
+
+	/* istanbul ignore next */
+	private async getSystemInfo (): Promise<SystemInfoData> {
+		const cpuData = await currentLoad()
+		const memData = await mem()
+		const diskData = await fsSize()
+		const diskTotalSize: number = diskData.reduce((prev, cur) => {
+			return prev + cur.size
+		}, 0)
+		const diskUsage: number = diskData.reduce((prev, cur) => {
+			return prev + cur.used
+		}, 0)
+		return {
+			cpu_usage_percent: cpuData.avgload,
+			disk_capacity: diskTotalSize,
+			disk_usage: diskUsage,
+			ram_available: memData.total,
+			ram_used: memData.used
 		}
 	}
 }
